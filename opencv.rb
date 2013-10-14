9b5993be1402b10d85a1c6a42c0acbe9b30ddaf6
Alex Brem <alex@fluktuation.net>
2013-10-14 03:50:47 +0200

    Update OpenCV to 2.4.6.2

--- opencv.rb
+++ opencv.rb
@@ -2,20 +2,19 @@ require 'formula'
 
 class Opencv < Formula
   homepage 'http://opencv.org/'
-  url 'http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.5/opencv-2.4.5.tar.gz'
-  sha1 '9e25f821db9e25aa454a31976ba6b5a3a50b6fa4'
-
-  env :std # to find python
+  url 'https://github.com/Itseez/opencv/archive/2.4.6.2.tar.gz'
+  sha1 'e599c984eabd16730df7f4ecfd7cd3778784e685'
 
   option '32-bit'
   option 'with-qt',  'Build the Qt4 backend to HighGUI'
   option 'with-tbb', 'Enable parallel code in OpenCV using Intel TBB'
   option 'with-opencl', 'Enable gpu code in OpenCV using OpenCL'
 
-  depends_on 'cmake' => :build
+  depends_on 'cmake'   => :build
   depends_on 'pkg-config' => :build
-  depends_on 'numpy' => :python
+  depends_on 'numpy'   => :python
 
+  depends_on 'python'  => :optional
   depends_on 'eigen'   => :optional
   depends_on 'libtiff' => :optional
   depends_on 'jasper'  => :optional
@@ -26,6 +25,7 @@ class Opencv < Formula
   # Can also depend on ffmpeg, but this pulls in a lot of extra stuff that
   # you don't need unless you're doing video analysis, and some of it isn't
   # in Homebrew anyway. Will depend on openexr if it's installed.
+  depends_on 'ffmpeg'  => [:build, :optional]
 
   def install
     args = std_cmake_args + %w[
@@ -46,37 +46,16 @@ class Opencv < Formula
     end
     args << '-DWITH_QT=ON' if build.with? 'qt'
     args << '-DWITH_TBB=ON' if build.with? 'tbb'
-    args << '-DWITH_OPENCL=ON' if build.with? 'opencl'
-
-    # The CMake `FindPythonLibs` Module is dumber than a bag of hammers when
-    # more than one python installation is available---for example, it clings
-    # to the Header folder of the system Python Framework like a drowning
-    # sailor.
-    #
-    # This code was cribbed from the VTK formula and uses the output to
-    # `python-config` to do the job FindPythonLibs should be doing in the first
-    # place.
-    python_prefix = `python-config --prefix`.strip
-    # Python is actually a library. The libpythonX.Y.dylib points to this lib, too.
-    if File.exist? "#{python_prefix}/Python"
-      # Python was compiled with --framework:
-      args << "-DPYTHON_LIBRARY='#{python_prefix}/Python'"
-      if !MacOS::CLT.installed? and python_prefix.start_with? '/System/Library'
-        # For Xcode-only systems, the headers of system's python are inside of Xcode
-        args << "-DPYTHON_INCLUDE_DIR='#{MacOS.sdk_path}/System/Library/Frameworks/Python.framework/Versions/2.7/Headers'"
-      else
-        args << "-DPYTHON_INCLUDE_DIR='#{python_prefix}/Headers'"
-      end
+    # OpenCL 1.1 is required, but Snow Leopard and older come with 1.0
+    args << '-DWITH_OPENCL=OFF' if build.without? 'opencl' or MacOS.version < :lion
+    if build.with? 'python'
+      args << "-DPYTHON_INCLUDE_DIR='#{python.incdir}'"
+      args << "-DPYTHON_LIBRARY='#{python.libdir}/lib#{python.xy}.dylib'"
+      args << "-DPYTHON_EXECUTABLE='#{python.binary}'"
     else
-      python_lib = "#{python_prefix}/lib/lib#{which_python}"
-      if File.exists? "#{python_lib}.a"
-        args << "-DPYTHON_LIBRARY='#{python_lib}.a'"
-      else
-        args << "-DPYTHON_LIBRARY='#{python_lib}.dylib'"
-      end
-      args << "-DPYTHON_INCLUDE_DIR='#{python_prefix}/include/#{which_python}'"
+      args << "-DWITH_PYTHON=OFF"
     end
-    args << "-DPYTHON_PACKAGES_PATH='#{lib}/#{which_python}/site-packages'"
+    args << '-DWITH_FFMPEG=OFF' unless build.with? 'ffmpeg'
 
     args << '..'
     mkdir 'macbuild' do
@@ -86,19 +65,7 @@ class Opencv < Formula
     end
   end
 
-  def which_python
-    "python" + `python -c 'import sys;print(sys.version[:3])'`.strip
-  end
-
-  def site_package_dir
-    "lib/#{which_python}/site-packages"
-  end
-
-  def caveats; <<-EOS.undent
-    The OpenCV Python module will not work until you edit your PYTHONPATH like so:
-      export PYTHONPATH="#{HOMEBREW_PREFIX}/#{site_package_dir}:$PYTHONPATH"
-
-    To make this permanent, put it in your shell's profile (e.g. ~/.profile).
-    EOS
+  def caveats
+    python.standard_caveats if python
   end
 end
