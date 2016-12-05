aa7586b1bf5b358452c3519a92f52254cf6e6926
Alex Brem <alex@fluktuation.net>
2016-12-05 19:54:00 +0100

    Update lftp to version 4.7.4

--- lftp.rb
+++ lftp.rb
@@ -1,10 +1,12 @@
 require "formula"
 
 class Lftp < Formula
-  homepage "http://lftp.yar.ru/"
-  url "http://lftp.yar.ru/ftp/lftp-4.4.16.tar.xz"
-  sha1 "4426801c8f08838a02dbd790eb7d8d75c460e445"
+  desc "Sophisticated file transfer program"
+  homepage "http://lftp.tech/"
+  url "http://lftp.yar.ru/ftp/lftp-4.7.4.tar.xz"
+  sha256 "bf67c4d128b6f769a4082947376a9679c5ee3463a24ab761a0757f75d70bd92c"
 
+  depends_on "xz" => :build
   depends_on "pkg-config" => :build
   depends_on "readline"
   depends_on "openssl"
